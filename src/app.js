5311c71e4883ff9f729b13e32ffd274a8f591462
harsha-namaste <mharshar2000@gmail.com>
2025-10-12 20:36:08 +0530

    Exploring Get, Post and Delete

--- src/app.js
+++ src/app.js
@@ -4,15 +4,21 @@ const express = require("express");
 
 const app = express();
 
-app.use("/home",(req, res) => {
-  res.send(
-    "<h1>Hello from express</h1>"
-  );
+app.get("/user", (req, res) => {
+    res.send({firstName: "Harsha", lastName: "jain"});
 });
 
-app.use("/", (req, res) => {
+app.post("/user", (req, res) => {
+    res.send("Data Posted successfully");
+});
+
+app.delete("/user", (req, res) => {
+    res.send("Data Deleted successfully");
+})
+
+app.use("/home", (req, res) => {
   res.send(
-    "<h1>Hello from Dashboard</h1>"
+    "<h1>Hello from express</h1>"
   );
 });
 
